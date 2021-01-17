13791bb110697472799df6ee51fdc31172d85bd5
dartblack <dartblack48@gmail.com>
2021-01-18 00:26:26 +0400

    start project

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ DR2 = OutputDevice(17)
 PL2 = OutputDevice(18)
 
 
-def top_construct(dr, duration=100, delay=0.00001):
+def top_construct(dr, duration=5000, delay=0.0000001):
     if dr == 1:
         DR1.on()
         DR2.off()
