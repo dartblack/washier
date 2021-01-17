c5332ad5e024539dc49598cdbceed48c6bc29590
dartblack <dartblack48@gmail.com>
2021-01-18 00:03:10 +0400

    start project

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 from gpiozero import OutputDevice
-from gpiozero import PWMOutputDevice
 from time import sleep
 
 DR1 = OutputDevice(14)
-PL1 = PWMOutputDevice(15, True, 0, 100000)
+PL1 = OutputDevice(15)
 
 DR2 = OutputDevice(17)
-PL2 = PWMOutputDevice(18, True, 0, 100000)
+PL2 = OutputDevice(18)
 
 
-def top_construct(dr, duration=5000, delay=0.00001):
+def top_construct(dr, duration=100, delay=0.00001):
     if dr == 1:
         DR1.on()
         DR2.off()
     elif dr == 2:
         DR1.off()
         DR2.on()
+
     for i in range(duration):
-        PL1.pulse(delay, delay)
-        PL2.pulse(delay, delay)
+        PL1.off()
+        PL2.off()
+        sleep(delay)
+        PL1.on()
+        PL2.on()
+        sleep(delay)
 
 
 print("Start move forward")
-top_construct(1)
-sleep(5)
+top_construct(1, 100000)
 print("End move")
