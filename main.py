095d5968c0924d2b1d90a0f7329b7dab67fa3814
dartblack <dartblack48@gmail.com>
2021-01-28 11:11:18 +0400

    start project

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from gpiozero import OutputDevice
 from gpiozero import DistanceSensor
 from time import sleep
 
+top_motor = {
+    "DR1": OutputDevice(14),
+    "PL1": OutputDevice(15),
+    "DR2": OutputDevice(17),
+    "PL2": OutputDevice(18)
+}
+
 DR1 = OutputDevice(14)
 PL1 = OutputDevice(15)
 
@@ -17,20 +24,20 @@ def read_distance_sensor(index):
     return sensor[index].distance * 100
 
 
-def top_construct(dr, duration=5000, delay=0.0000001):
+def top_motor_control(dr, duration=1000, delay=0.0000001):
     if dr == 1:
-        DR1.on()
-        DR2.off()
+        top_motor["DR1"].on()
+        top_motor["DR2"].off()
     elif dr == 2:
-        DR1.off()
-        DR2.on()
+        top_motor["DR1"].off()
+        top_motor["DR2"].on()
 
     for i in range(duration):
-        PL1.on()
-        PL2.on()
+        top_motor["PL1"].on()
+        top_motor["PL2"].on()
         sleep(delay)
-        PL1.off()
-        PL2.off()
+        top_motor["PL1"].off()
+        top_motor["PL2"].off()
         sleep(delay)
 
 
