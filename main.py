7ccc62b7829e15ae7a74cae7c6fc448c9e684413
dartblack <dartblack48@gmail.com>
2021-02-11 16:44:30 +0400

    start project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from gpiozero import OutputDevice
 from gpiozero import DistanceSensor
-from gpiozero import Button
+from gpiozero import DigitalInputDevice
 from time import sleep
 
 direct_width = 600
@@ -26,12 +26,12 @@ side_motor = {
 }
 
 safe_sensors = {
-    "1.1": Button(5),
-    "1.2": Button(6),
-    "2.1": Button(12),
-    "2.2": Button(13),
-    "3.1": Button(19),
-    "3.2": Button(20)
+    "1.1": DigitalInputDevice(5),
+    "1.2": DigitalInputDevice(6),
+    "2.1": DigitalInputDevice(12),
+    "2.2": DigitalInputDevice(13),
+    "3.1": DigitalInputDevice(19),
+    "3.2": DigitalInputDevice(20)
 }
 
 
