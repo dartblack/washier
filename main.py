c2420684770da7ea9fc62346c89d75b6a42a7ec6
dartblack <dartblack48@gmail.com>
2021-02-11 16:36:33 +0400

    start project

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ def construct_calibration():
 print("start...")
 
 while True:
-    print(str(safe_sensors['1.2'].value))
+    print(str(safe_sensors['1.2'].is_active))
     sleep(0.5)
 
 # top_distance = read_distance_sensor("top_sensor")
