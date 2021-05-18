1b470353e1ad6fa66add8ed7f525c7324fe4ed66
dartblack <dartblack48@gmail.com>
2021-05-18 21:49:48 +0400

    start project

--- Sensors/DistanceSensor.py
+++ Sensors/DistanceSensor.py
@@ -26,28 +26,19 @@ class Distance:
         time.sleep(0.00001)
         self.trigger.off()
 
-        count = self.count
-        while True:
+        end = True
+        while end:
             for k in self.keys:
-                print(self.echos[k].value)
-                if self.echos[k].value == 0 and self.startTime[k] == 0:
+                if self.echos[k].value == 0:
+                    end = False
                     self.startTime[k] = time.time()
-                    count = count - 1
-            print(self.startTime)
-            if count == 0:
-                break
 
-        count2 = self.count
-        while True:
-            print('start end')
+        end = True
+        while end:
             for k in self.keys:
-                print(self.echos[k].value)
-                if self.echos[k].value == 1 and self.stopTime[k] == 0:
+                if self.echos[k].value == 1:
+                    end = False
                     self.stopTime[k] = time.time()
-                    count2 = count2 - 1
-            print(self.stopTime)
-            if count2 == 0:
-                break
 
         for k in self.keys:
             TimeElapsed = self.stopTime[k] - self.startTime[k]
